545ff4c197ef41828d9d64339aad93b3f8318ac1
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-07 00:56:02 -0300

    feat(Home): add page basic layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,54 @@
+import { useState } from 'react'
+
+import Card from 'components/Card'
+import Modal from 'components/Modal'
+import DatePicker from 'components/DatePicker'
+import Input from 'components/Input'
+import Textarea from 'components/Textarea'
+import Button from 'components/Button'
+
 import * as S from './styles'
 
 const Home = (): React.ReactElement => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [calendarDate, setCalendarDate] = useState<Date | null>(new Date())
+
   return (
     <S.Wrapper>
-      <h1>Home</h1>
+      <div className="w__grid">
+        <Card
+          mode="display"
+          eventDate={new Date()}
+          description="descrição"
+          guestsNumber={20}
+          amount={200}
+        />
+        <Card mode="link" onClick={() => setModalIsOpen(true)} />
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onClose={() => setModalIsOpen(false)}
+        modalTitle="Adicionar um novo churras"
+      >
+        <form action="" className="wm__form">
+          <Input
+            label="Titulo"
+            placeholder="Qual o motivo da festa?"
+            autoFocus
+          />
+          <DatePicker
+            selected={calendarDate}
+            onChange={(date) => setCalendarDate(date)}
+            label="Selecione uma data"
+            placeholderText="Escolha uma boa data para celebrar!"
+          />
+          <Textarea
+            label="Informações adicionais"
+            placeholder="O que o pessoal não pode esquecer?"
+          />
+          <Button fullWidth>Adicionar</Button>
+        </form>
+      </Modal>
     </S.Wrapper>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  .w__grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-auto-rows: 1fr;
+    gap: 2.4rem;
+  }
+
+  .wm__form {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 4rem;
+
+    width: 100%;
+  }
+`
