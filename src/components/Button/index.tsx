256cd9254db2c5511250740ed6eb910ac9b3cc20
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-08 17:19:11 -0300

    feat(Button): add icon prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,16 +5,19 @@ export type ButtonVariants = 'default' | 'ghost'
 export type ButtonProps = {
   fullWidth?: boolean
   variant?: ButtonVariants
+  icon?: React.ReactNode
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 const Button = ({
   fullWidth,
   variant = 'default',
+  icon,
   children,
   ...props
 }: ButtonProps): React.ReactElement => {
   return (
     <S.Button fullWidth={fullWidth} variant={variant} type="button" {...props}>
+      {icon}
       {children}
     </S.Button>
   )
