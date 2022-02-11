582ed9c63e7da84f57eba51c027c7dd47812ad21
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-11 16:23:59 -0300

    feat(Textarea): add final component logic

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -1,27 +1,26 @@
-import { forwardRef } from 'react'
-
 import * as S from './styles'
 
 type TextareaProps = {
   label: string
 } & React.TextareaHTMLAttributes<HTMLTextAreaElement>
 
-export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ label, ...props }, ref): React.ReactElement => {
-    const linkTextareaLabel = label.replace(/\W/g, '').toLowerCase()
+export const Textarea = ({
+  label,
+  ...props
+}: TextareaProps): React.ReactElement => {
+  const linkTextareaLabel = label.replace(/\W/g, '').toLowerCase()
 
-    return (
-      <S.Wrapper>
-        <label htmlFor={linkTextareaLabel} className="w__label">
-          {label}
-        </label>
-        <textarea
-          ref={ref}
-          id={linkTextareaLabel}
-          className="w__textarea"
-          {...props}
-        />
-      </S.Wrapper>
-    )
-  }
-)
+  return (
+    <S.Wrapper>
+      <label role="label" htmlFor={linkTextareaLabel} className="w__label">
+        {label}
+      </label>
+      <textarea
+        role="textarea"
+        id={linkTextareaLabel}
+        className="w__textarea"
+        {...props}
+      />
+    </S.Wrapper>
+  )
+}
