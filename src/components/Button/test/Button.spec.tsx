5cd17d3c1275bf8c11efdd4c931f5526c9969b05
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-11 04:22:41 -0300

    feat(Button): add new variant in component

--- src/components/Button/test/Button.spec.tsx
+++ src/components/Button/test/Button.spec.tsx
@@ -1,10 +1,10 @@
-import { renderWithTheme } from 'utils/render-with-theme'
+import { renderWithTheme } from 'shared/utils/render-with-theme'
 
 import { Button } from '..'
 
 describe('<Button />', () => {
   it('Should render', () => {
-    renderWithTheme(<Button />)
+    renderWithTheme(<Button variant="default" />)
     expect(1).toBe(1)
   })
 })
