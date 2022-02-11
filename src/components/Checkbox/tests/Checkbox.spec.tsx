005d0814910f3318404ef66dd9ec68800c419761
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-11 16:23:01 -0300

    test(Checkbox): add final component logic

--- src/components/Checkbox/tests/Checkbox.spec.tsx
+++ src/components/Checkbox/tests/Checkbox.spec.tsx
@@ -1,9 +1,33 @@
+import '@testing-library/jest-dom/extend-expect'
+import { screen } from '@testing-library/react'
+
 import { renderWithTheme } from 'shared/utils/render-with-theme'
 
 import { Checkbox } from '..'
 
 describe('<Checkbox />', () => {
-  it('render Checkbox', () => {
-    renderWithTheme(<Checkbox label="test" />)
+  it('Should render correct label', () => {
+    const textContent = 'test'
+
+    renderWithTheme(<Checkbox label={textContent} />)
+
+    const label = screen.getByRole('label')
+
+    expect(label).toHaveTextContent(textContent)
+  })
+
+  it('Should render iconInfo', () => {
+    const iconContent = <span>test</span>
+
+    renderWithTheme(
+      <Checkbox
+        label="test"
+        infoIcon={{ icon: iconContent, message: 'test icon' }}
+      />
+    )
+
+    const icon = screen.getByRole('icon')
+
+    expect(icon).toBeInTheDocument()
   })
 })
