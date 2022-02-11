a7ea70e8d6f42ecf4e1efe1878b466abc8bc1b93
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-11 16:24:29 -0300

    feat(Input): add final component logic

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,22 +1,18 @@
-import { forwardRef } from 'react'
-
 import * as S from './styles'
 
 type InputProps = {
   label: string
 } & React.InputHTMLAttributes<HTMLInputElement>
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, ...props }, ref): React.ReactElement => {
-    const linkInputLabel = label.replace(/\W/g, '').toLowerCase()
+export const Input = ({ label, ...props }: InputProps): React.ReactElement => {
+  const linkInputLabel = label.replace(/\W/g, '').toLowerCase()
 
-    return (
-      <S.Wrapper>
-        <label htmlFor={linkInputLabel} className="w__label">
-          {label}
-        </label>
-        <input ref={ref} id={linkInputLabel} className="w__input" {...props} />
-      </S.Wrapper>
-    )
-  }
-)
+  return (
+    <S.Wrapper>
+      <label role="label" htmlFor={linkInputLabel} className="w__label">
+        {label}
+      </label>
+      <input role="input" id={linkInputLabel} className="w__input" {...props} />
+    </S.Wrapper>
+  )
+}
