ecc4f402720909971c0bc6752844f09c0ddbf914
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-11 04:24:34 -0300

    refactor: set correct importation of util

--- src/components/CurrencyInput/tests/CurrencyInput.spec.tsx
+++ src/components/CurrencyInput/tests/CurrencyInput.spec.tsx
@@ -1,9 +1,9 @@
-import { renderWithTheme } from 'utils/render-with-theme'
+import { renderWithTheme } from 'shared/utils/render-with-theme'
 
 import { CurrencyInput } from '..'
 
 describe('<CurrencyInput />', () => {
   it('render CurrencyInput', () => {
-    renderWithTheme(<CurrencyInput label="test" />)
+    renderWithTheme(<CurrencyInput label="test" value={0} />)
   })
 })
