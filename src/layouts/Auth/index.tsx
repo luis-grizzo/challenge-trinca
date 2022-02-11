4661f4dccbafbcc645762a219818a25a9fa1eab0
Luís Grizzo <luis.grizzo@liveonsolutions.com>
2022-02-11 16:21:40 -0300

    feat(Auth): add layout final logic version

--- src/layouts/Auth/index.tsx
+++ src/layouts/Auth/index.tsx
@@ -1,20 +1,38 @@
 import { Outlet } from 'react-router-dom'
+import { useSpring, animated } from 'react-spring'
 
 import logo_trinca from 'assets/logo_trinca.svg'
 
 import * as S from './styles'
 
 const AuthLayout = (): React.ReactElement => {
+  const titleAnimation = useSpring({
+    from: { opacity: 0, y: -300 },
+    to: { opacity: 1, y: 0 }
+  })
+
+  const footerAnimation = useSpring({
+    from: { opacity: 0, y: 300 },
+    to: { opacity: 1, y: 0 }
+  })
+
   return (
     <S.Body>
       <div className="b__header">
-        <h1 className="bh__title">Agenda de Churras</h1>
+        <animated.h1 style={titleAnimation} className="bh__title">
+          Agenda de Churras
+        </animated.h1>
       </div>
       <div className="b__page-container">
         <Outlet />
       </div>
       <footer className="b__footer">
-        <img src={logo_trinca} alt="Trinca's logo" className="bf__image" />
+        <animated.img
+          style={footerAnimation}
+          src={logo_trinca}
+          alt="Trinca's logo"
+          className="bf__image"
+        />
       </footer>
     </S.Body>
   )
